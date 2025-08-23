0b13cf6b5f32d5ce3de4e5e5f0b45347ac44aba6
Habeebah <ojuolapehabeebah@gmail.com>
2025-08-23 07:12:32 +0100

    Add shopping list functionality with local storage support
    - Introduced a new PNG file for the shopping list icon.
    - Added CSS styles for the big image class.
    - Updated HTML to include input fields for item and quantity, along with buttons for adding, editing, and removing items.
    - Implemented TypeScript logic for managing the shopping list, including adding items, removing items with confirmation, and editing items (currently a placeholder).
    - Integrated local storage to persist the shopping list data across sessions.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,41 @@
+import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('shopping-list-app');
+  
+  shoppingList = "";
+  shoppingListQuantity = "";
+  shoppingLists:Array<{item: string, quantity: string}> = [];
+  addItem(){
+    this.shoppingLists.push({item: this.shoppingList, quantity: this.shoppingListQuantity});
+    console.log(this.shoppingLists);
+    localStorage.setItem('shoppingList', JSON.stringify(this.shoppingLists));
+    this.shoppingList = "";
+    this.shoppingListQuantity = "";
+  }
+
+  removeItem(index: number) {
+    const delItem = confirm("Are you sure you want to delete this item?");
+    if( delItem === true){
+    this.shoppingLists.splice(index, 1);
+    localStorage.setItem('shoppingList', JSON.stringify(this.shoppingLists));
+    }  
+  }
+
+  editItem(index: number) {
+    alert("working on it ")
+    // const item = this.shoppingLists[index];
+    // this.shoppingList = item.item;
+    // this.shoppingListQuantity = item.quantity;
+    // this.removeItem(index);
+  }
+
 }
