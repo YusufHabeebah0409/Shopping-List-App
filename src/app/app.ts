25dc8de7c70d6198836c7945cc1d3ce8698cc26d
Habeebah <ojuolapehabeebah@gmail.com>
2025-08-23 20:50:54 +0100

    Implement local storage support for shopping list; add item validation, update favicon, and include Bootstrap for styling

--- src/app/app.ts
+++ src/app/app.ts
@@ -10,25 +10,37 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  
+
+  ngOnInit() {
+    const storedList = localStorage.getItem('shoppingList');
+    if (storedList) {
+      this.shoppingLists = JSON.parse(storedList);
+    }
+  }
+
   shoppingList = "";
   shoppingListQuantity = "";
-  shoppingLists:Array<{item: string, quantity: string}> = [];
-  addItem(){
-    this.shoppingLists.push({item: this.shoppingList, quantity: this.shoppingListQuantity});
-    console.log(this.shoppingLists);
-    localStorage.setItem('shoppingList', JSON.stringify(this.shoppingLists));
-    this.shoppingList = "";
-    this.shoppingListQuantity = "";
+  shoppingLists: Array<{ item: string, quantity: string }> = [];
+  addItem() {
+    if (this.shoppingList === "" || this.shoppingListQuantity === "") {
+      alert("Please enter both item and quantity/price.");
+    } else {
+        this.shoppingLists.push({ item: this.shoppingList, quantity: this.shoppingListQuantity });
+        console.log(this.shoppingLists);
+        localStorage.setItem('shoppingList', JSON.stringify(this.shoppingLists));
+        this.shoppingList = "";
+        this.shoppingListQuantity = "";
+      }
   }
+
 
   removeItem(index: number) {
     const delItem = confirm("Are you sure you want to delete this item?");
-    if( delItem === true){
-    this.shoppingLists.splice(index, 1);
-    localStorage.setItem('shoppingList', JSON.stringify(this.shoppingLists));
+    if (delItem === true) {
+      this.shoppingLists.splice(index, 1);
+      localStorage.setItem('shoppingList', JSON.stringify(this.shoppingLists));
     }
-  
+
   }
 
   editItem(index: number) {
